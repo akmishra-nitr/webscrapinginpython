74f07742a1ceb3b07089ccd3e5826576f066c672
Arun <akmishralive@live.com>
2019-03-11 11:05:11 +0100

    Modified References, added simple script
    Moved references from README to References and added a simple Python script to return a CSV file containing needed data from a website

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # webscrapinginpython
 Simple Python scripts for web scraping
-
-Reference links:
-First web scraper (https://first-web-scraper.readthedocs.io/en/latest/)
-BeautifulSoup Documentation (https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
---
+++ simplewebscraper.py
@@ -0,0 +1,24 @@
+# simplewebscraper.py
+
+# This script is only used for information purpose to demonstrate how a simple web scraper works.
+# It scrapes the IMDB page of actor Morgan Freeman (a really good actor) and returns the films in which he has acted along with the name of his character and the year the film was produced.
+
+# Import relevant libraries
+import re
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.imdb.com/name/nm0000151/?ref_=nv_sr_1'
+response = requests.get(url)
+html = response.content
+
+# soup = BeautifulSoup(html) Not recommended for Python 3
+soup = BeautifulSoup(html, 'html.parser')
+
+# Open file to write, read the html file using regular expressions to find div classes with id as actor and then print the relevant contents to the file
+with open('file.csv', 'a') as f:
+	for div in soup.findAll('div', id=re.compile('^actor-')):
+		print(str(div.contents[1].contents[0].strip()) + "," + str(div.contents[3].contents[0].contents[0].strip()) + "," + str(div.contents[6].strip()))
+
+# Close output stream
+f.close()
