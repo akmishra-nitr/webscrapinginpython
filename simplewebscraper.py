ba63704ac716f5811ae04c71cf931c6c31aa81d0
Arun <akmishralive@live.com>
2019-04-30 15:35:05 +0200

    Updated write command to file
    Updated write command to file

--- simplewebscraper.py
+++ simplewebscraper.py
@@ -18,7 +18,7 @@ soup = BeautifulSoup(html, 'html.parser')
 # Open file to write, read the html file using regular expressions to find div classes with id as actor and then print the relevant contents to the file
 with open('file.csv', 'a') as f:
 	for div in soup.findAll('div', id=re.compile('^actor-')):
-		print(str(div.contents[1].contents[0].strip()) + "," + str(div.contents[3].contents[0].contents[0].strip()) + "," + str(div.contents[6].strip()))
+		f.write(str(div.contents[1].contents[0].strip()) + "," + str(div.contents[3].contents[0].contents[0].strip()) + "," + str(div.contents[6].strip()))
 
 # Close output stream
 f.close()
